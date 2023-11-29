18a2114ab759ae8a2b82e357cd687b72000b56b7
marjanOSD <marjan.cse.20220104106@aust.edu>
2023-11-30 00:34:28 +0600

    error

--- lib/secondPage.dart
+++ lib/secondPage.dart
@@ -18,18 +18,24 @@ class _SecondState extends State<Second> {
       body: Column(
         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
         children: [
-          GestureDetector(onTap:(){
-            showDialog(context: context, builder:(BuildContext Context){ return Expanded(child:AlertDialog(title: Text("Bkash"),content: Text("Payment successful"),));});
-          },child: Image.asset("assets/images/bkash.webp",height: 60,width: 60,)),
-          GestureDetector(onTap:(){ showDialog(context: context, builder:(BuildContext Context){ return Expanded(child:AlertDialog(title: Text("Rocket"),content: Text("Payment successful"),));});},child: Image.asset("assets/images/rocket.png",height: 60,width: 60,)),
-          GestureDetector(onTap:(){ showDialog(context: context, builder:(BuildContext Context){ return Expanded(child:AlertDialog(title: Text("Amex"),content: Text("Payment successful"),));});},child: Image.asset("assets/images/amex.png",height: 60,width: 60,)),
-          GestureDetector(onTap:(){ showDialog(context: context, builder:(BuildContext Context){ return Expanded(child:AlertDialog(title: Text("VISA"),content: Text("Payment successful"),));});},child: Image.asset("assets/images/visa.png",height: 60,width: 60,)),
-          GestureDetector(onTap:(){ showDialog(context: context, builder:(BuildContext Context){ return Expanded(child:AlertDialog(title: Text("MasterCard"),content: Text("Payment successful"),));});},child: Image.asset("assets/images/mastercard.png",height: 60,width: 60,)),
+          Text("Acceptable: ",style: TextStyle(fontWeight: FontWeight.bold,fontSize: 21),),
+          SizedBox.fromSize(size: Size(0,40),),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+
+            children: [
+              GestureDetector(onTap:(){
+                showDialog(context: context, builder:(BuildContext Context){ return Expanded(child:AlertDialog(title: Text("Bkash"),content: Text("Payment successful"),));});
+              },child: Image.asset("assets/images/bkash.webp",height: 60,width: 60,)),
+              GestureDetector(onTap:(){ showDialog(context: context, builder:(BuildContext Context){ return Expanded(child:AlertDialog(title: Text("Rocket"),content: Text("Payment successful"),));});},child: Image.asset("assets/images/rocket.png",height: 60,width: 60,)),
+              GestureDetector(onTap:(){ showDialog(context: context, builder:(BuildContext Context){ return Expanded(child:AlertDialog(title: Text("Amex"),content: Text("Payment successful"),));});},child: Image.asset("assets/images/amex.png",height: 60,width: 60,)),
+              GestureDetector(onTap:(){ showDialog(context: context, builder:(BuildContext Context){ return Expanded(child:AlertDialog(title: Text("VISA"),content: Text("Payment successful"),));});},child: Image.asset("assets/images/visa.png",height: 60,width: 60,)),
+              GestureDetector(onTap:(){ showDialog(context: context, builder:(BuildContext Context){ return Expanded(child:AlertDialog(title: Text("MasterCard"),content: Text("Payment successful"),));});},child: Image.asset("assets/images/mastercard.png",height: 60,width: 60,)),
 
 
-        ],
-      ),
-      // SizedBox.fromSize(size: Size(0,40),),
+            ],
+          ),
+          // SizedBox.fromSize(size: Size(0,40),),
         ],
       ),
     );
