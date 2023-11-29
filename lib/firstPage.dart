3c4454a9dd14213d49ae1ce8200f93d105a7e753
marjanOSD <marjan.cse.20220104106@aust.edu>
2023-11-30 00:23:05 +0600

    image and details added

--- lib/firstPage.dart
+++ lib/firstPage.dart
@@ -9,7 +9,26 @@ class First extends StatelessWidget {
       appBar: AppBar(
         title: Text("First Page"),
       ),
-      body: Column(children: [
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Image.asset("assets/images/dress.jpg",height: 400,width: 400,),
+          SizedBox.fromSize(size: Size(0, 10),),
+          Padding(
+            padding:  EdgeInsets.symmetric(horizontal: 25),
+            child: Text("৳ 3,564.75",style: TextStyle(fontWeight: FontWeight.bold,fontSize: 25,color: Colors.red),),
+          ),
+          Padding(
+            padding:  EdgeInsets.symmetric(horizontal: 13),
+            child: Text("Womens Salwor Kameez",style: TextStyle(fontWeight: FontWeight.bold,fontSize: 30),),
+          ),
+          SizedBox.fromSize(size: Size(0, 10),),
+
+
+          Padding(
+            padding:  EdgeInsets.symmetric(horizontal: 16),
+            child: Text("– Matt Pink Color\n – Viscose\n – 3 piec",style: TextStyle(fontWeight: FontWeight.bold,fontSize: 22,color: Colors.black54),),
+          ),
       ],),
     );
   }
